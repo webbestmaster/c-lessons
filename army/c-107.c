4e1b09795a615e155ab3fbf8e5b9aacdf56ac556
dmitryturovtsov <web.best.master@gmail.com>
2019-11-07 21:11:27 +0300

    c-107 - begin

---
+++ army/c-107.c
@@ -0,0 +1,10 @@
+// как запускать
+// gcc ./c-107.c && ./a.out
+// эта строка скомпилит "c-107.c" и запусть программу "a.out"
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    return 0;
+}
