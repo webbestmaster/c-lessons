7b6fafdb05fc55d471c878d87bdea5e4da630f5b
dmitryturovtsov <web.best.master@gmail.com>
2019-11-08 14:52:10 +0300

    c-107 - end

--- army/c-107.c
+++ army/c-107.c
@@ -5,6 +5,44 @@
 #include <stdio.h>
 #include <math.h>
 
+int makeFile(char filePath[], char message[], float number) {
+    FILE *file = fopen(filePath, "w");
+
+    fprintf(file, message, number);
+
+    fclose(file);
+
+    return 0;
+}
+
 int main() {
+    FILE *myFile = fopen("ini.txt", "r");
+
+    int arrayLength = 1000;
+    int numberArray[arrayLength];
+    int currentNumber = 0;
+    int currentProgress = 1;
+    int numberListSize = 0;
+    float result = 0;
+
+    int i;
+
+    for (i = 0; i < arrayLength; i++) {
+        fscanf(myFile, "%d ", &currentNumber);
+        if (currentNumber == 0) {
+            numberListSize = i;
+            break;
+        } else {
+            currentProgress = currentProgress * currentNumber;
+            numberArray[i] = currentNumber;
+        }
+    }
+
+    fclose(myFile);
+
+    result = pow(currentProgress, 1.0 / numberListSize);
+
+    makeFile("out.txt", "Среднее геометрическое: %f\n", result);
+
     return 0;
 }
--- army/c-138.c
+++ army/c-138.c
@@ -4,7 +4,6 @@
 
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
 
 int makeFile(char filePath[], char message[], float number) {
     FILE *file = fopen(filePath, "w");
